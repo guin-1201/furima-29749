b8cba98a9af7914ac132004a1bcdbae11d7232c0
guin-1201 <yk.wakahoi@gmail.com>
2020-10-08 02:48:10 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, :email, :password, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday, presence: true
-
+  with_options presence: true do
+    validates :nicknanme
+    validates :email, format: { uniqueness: true, inclusion: { in: %w(@)}
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates :password, format: { length: { minimum: 7 }, with: PASSWORD_REGEX, message: '英字と数字の両方を含めて設定してください'}
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :birthday
+  end
+  
 end
