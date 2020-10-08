72c0f62b91aeaa9cdff748b6277dbda844d1e5ec
guin-1201 <yk.wakahoi@gmail.com>
2020-10-08 12:07:16 +0900

    pry-rails導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,15 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nicknanme
-    validates :email, format: { uniqueness: true, inclusion: { in: %w(@)}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, format: { with: VALID_EMAIL_REGEX, uniqueness: true, inclusion: { in: %w(@)}}
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-    validates :password, format: { length: { minimum: 7 }, with: PASSWORD_REGEX, message: '英字と数字の両方を含めて設定してください'}
+    validates :password, format: { length: { minimum: 7 }, with: PASSWORD_REGEX, message: 'passwordは英字と数字の両方を含めて設定してください'}
     validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
     validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
     validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
     validates :birthday
   end
-  
+
 end
