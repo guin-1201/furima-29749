49713f75b40a6ac87b231e7b5729a4e92c12f8cd
guin-1201 <yk.wakahoi@gmail.com>
2020-10-10 16:50:20 +0900

    itemコントローラー編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,22 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @tweet = Tweet.new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:product, :description, :category_id, :status_id, :shipping_charge_class_id, :ship_address_id, :ship_date_class_id, :price)
   end
 
 end
