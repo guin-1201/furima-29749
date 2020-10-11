843a5256b219c9d63bb3c6301acb9acd2a230d49
guin-1201 <yk.wakahoi@gmail.com>
2020-10-11 11:39:26 +0900

    ActiveHash記述・itemのファクトリーボット作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
 
   def index
+    @items = Item.all
   end
 
   def new
@@ -8,9 +9,11 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
-    if @item.save
-      redirect_to root_path
+    @item = Item.new(item_params)
+
+    if @item.valid?
+      @item.save
+      return redirect_to root_path
     else
       render :new
     end
@@ -21,6 +24,35 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :product, :description, :category_id, :status_id, :shipping_charge_class_id, :ship_address_id, :ship_date_class_id, :price)
   end
-
   
 end
+
+
+
+# class  DonationsController < ApplicationController
+#   def index
+#   end
+
+#   def new
+#     @donation = UserDonation.new   #「UserDonation」に編集
+#   end
+
+#   def create
+#     @donation = UserDonation.new(donation_params)   #「UserDonation」に編集
+    
+#     if @donation.valid?
+#       @donation.save  # バリデーションをクリアした時
+#       return redirect_to root_path
+#     else
+#       render "new"    # バリデーションに弾かれた時
+#     end
+#   end
+ 
+#   private
+ 
+#   def donation_params
+#    #「住所」「寄付金」のキーも追加
+#    params.require(:user_donation).permit(:name, :name_reading, :nickname, :postal_code, :prefecture, :city, :house_number, :building_name, :price)
+#   end
+ 
+#  end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :item do
-    
+    product                    { "たまご" }
+    description                {Faker::Lorem.sentence}
+    category_id                { "1" }
+    status_id                  { "2" }
+    shipping_charge_class_id   { "1" }
+    ship_address_id            { "2" }
+    ship_date_class_id         { "1" }
+    price                      { "1000" }
+
+    association :user   ##users.rbのFactoryBotとアソシエーションがある
+
   end
 end
