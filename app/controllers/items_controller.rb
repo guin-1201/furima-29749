e00d422066a41cc318356b198b24faacff8f95d4
guin-1201 <yk.wakahoi@gmail.com>
2020-10-13 17:59:58 +0900

    修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,12 +25,12 @@ class ItemsController < ApplicationController
   def show
   end
 
+  
+  private
+
   def set_item
     @item = Item.find(params[:id])
   end
-
-
-  private
 
   def item_params
     params.require(:item).permit(:image, :product, :description, :category_id, :status_id, :shipping_charge_class_id, :ship_address_id, :ship_date_class_id, :price).merge(user_id: current_user.id)
