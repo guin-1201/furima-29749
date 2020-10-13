713a962d8df864f9054687e4a7076f7a146a2afb
guin-1201 <yk.wakahoi@gmail.com>
2020-10-13 20:45:35 +0900

    削除機能：コントローラー記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
+  # before_action :move_to_index, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:edit, :show, :update, :destroy]
-
+  
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -34,7 +35,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
+    if current_user.id == @item.user.id
+      @item.destroy 
+    end
     return redirect_to root_path
   end
 
@@ -49,9 +52,9 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :product, :description, :category_id, :status_id, :shipping_charge_class_id, :ship_address_id, :ship_date_class_id, :price).merge(user_id: current_user.id)
   end
   
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
+  # def move_to_index
+  #   unless user_signed_in?
+  #     redirect_to action: :index
+  #   end
+  # end
 end
