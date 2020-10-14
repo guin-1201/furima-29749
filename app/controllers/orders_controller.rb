cb512fca4c973f25899c290f750aa1cff239a5b8
guin-1201 <yk.wakahoi@gmail.com>
2020-10-14 17:13:28 +0900

    購入画面に商品情報を表示できるように

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,9 +2,17 @@ class OrdersController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
 
   def index
+    @item = Item.find(params[:item_id])
+    @order = Purchase.new
   end
 
   def create
+    @order = Purchase.new(order_params)
+    if @order.valid?
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
   end
 
 end
@@ -12,9 +20,15 @@ end
 
 private
 
+  #途中　アドレステーブルからもひっぱってくる
+  def order_params
+    params.require(:purchase).permit(:user_id, :item_id)
+  end
+
   def move_to_index
     unless user_signed_in?
       return redirect_to new_user_session_path
     end
   end
+
 end
