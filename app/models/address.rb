cb46392b3ac429c33d903606349c5d2a92d15b01
guin-1201 <yk.wakahoi@gmail.com>
2020-10-14 12:00:10 +0900

    addressモデルのバリデーション設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,5 +4,13 @@ class Address < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
 
+  with_options presence: true do
+    validates :postal_code
+    validates :prefecture_id, numericality: { other_than: 0 }
+    validates :city
+    validates :house_number
+    validates :string
+  end
+
 
 end
