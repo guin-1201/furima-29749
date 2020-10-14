7f40e3fa73ee3284b23c17669a5e0b814a2db7f5
guin-1201 <yk.wakahoi@gmail.com>
2020-10-15 03:03:38 +0900

    クレジットカードの情報を取り除く

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -18,6 +18,15 @@ const pay = () => {
     };
 
     Payjp.createToken(card, (status, response) => {
+    //Payjp.createToken(card, callback)
+    //カード情報をPAY.JP側に送りトークン化するためpay.jsが提供するオブジェクト
+    //第一引数は先ほど定義したcardオブジェクト
+    //第二引数はAY.JP側からトークンが送付された後に実行する処理を、アロー関数を用いた即時関数として記述
+    //即時関数とは、読み込まれると（この場合ではPAY.JPからのレスポンスがあると）即時に実行される関数
+    // アロー関数 ＊functionの記述を省略
+    //const 変数名 = () => {
+    //  処理
+    //}
       if (status == 200) {
         const token = response.id;
       //statusはトークンの作成がうまくなされたかどうかを確認できる、HTTPステータスコードが入る
@@ -35,6 +44,23 @@ const pay = () => {
       //第一引数には、要素のどこに描画するのかを指定（beforeendは内部の最後の子要素の後に挿入、afterendは要素の直後に挿入、など）
       //第二引数には描画するHTML自体を渡す。予め変数に描画したいHTMLを代入しておく
     }
+
+    document.getElementById("card-number").removeAttribute("name");
+    document.getElementById("card-cvc").removeAttribute("name");
+    document.getElementById("card-exp-month").removeAttribute("name");
+    document.getElementById("card-exp-year").removeAttribute("name");
+    //フォームに存在するクレジットカードの各情報を削除
+    //要素.removeAttribute(name, value)
+    //nameは属性の名前を文字列で指定
+    //valueは属性に削除したい値を指定
+    //例）
+      //[HTML]
+      //<div class="contents" id="apple">りんご</div>
+      //[JS]
+      //const apple = document.getElementById("apple")
+      //apple.removeAttribute("class", "contents")
+      //=><div id="apple">りんご</div> が取得できる
+
     });
   });
 };
@@ -42,12 +68,3 @@ const pay = () => {
 window.addEventListener("load", pay);
 
 
-//Payjp.createToken(card, callback)
-//カード情報をPAY.JP側に送りトークン化するためpay.jsが提供するオブジェクト
-//第一引数は先ほど定義したcardオブジェクト
-//第二引数はAY.JP側からトークンが送付された後に実行する処理を、アロー関数を用いた即時関数として記述
-//即時関数とは、読み込まれると（この場合ではPAY.JPからのレスポンスがあると）即時に実行される関数
-// アロー関数 ＊functionの記述を省略
-//const 変数名 = () => {
-//  処理
-//}
