bed23e46602fd5c8d52c48401503074e45c09e48
guin-1201 <yk.wakahoi@gmail.com>
2020-10-14 12:24:34 +0900

    ordersコントローラー記述２

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,5 @@
 class OrdersController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
 
   def index
   end
@@ -6,4 +7,14 @@ class OrdersController < ApplicationController
   def create
   end
 
+end
+
+
+private
+
+  def move_to_index
+    unless user_signed_in?
+      return redirect_to new_user_session_path
+    end
+  end
 end
