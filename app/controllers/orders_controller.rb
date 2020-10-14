c1ef1a6efad8edce5c8c7623fab077ab129b99e8
guin-1201 <yk.wakahoi@gmail.com>
2020-10-14 20:53:47 +0900

    oderのindexビュー修正他

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -31,3 +31,4 @@ private
   end
 
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'items/index'
   devise_for :users
   root to: 'items#index'
-  resources :items
-  resources :orders
+  resources :items do
+    resources :orders, only: [:index, :create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
