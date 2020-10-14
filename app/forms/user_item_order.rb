20e5f2ef106bf99f719b4fdcc76769da89533a64
guin-1201 <yk.wakahoi@gmail.com>
2020-10-15 04:01:56 +0900

    tokenのバリデーションをUserItemOrderモデルに記述

--- app/forms/user_item_order.rb
+++ app/forms/user_item_order.rb
@@ -3,6 +3,7 @@ class UserItemOrder
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone, :purchase_id, :token
 
+  validates :token, presence: true
 
   def save
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone: phone, purchase_id: purchase_id)
